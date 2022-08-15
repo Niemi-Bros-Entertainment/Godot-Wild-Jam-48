44884dd5ad6ef23de548b5fec8cd9dff0fe4702e
Jonathan Niemi <jontniemi@gmail.com>
2022-08-15 15:29:01 -0400

    Created a Quit scene, increased moon resolution
    -quit scene delays application shutdown
    -increased resolution of procedural moon

--- Godot-Wild-Jam-48-Project/Scripts/Constants.gd
+++ Godot-Wild-Jam-48-Project/Scripts/Constants.gd
@@ -2,6 +2,6 @@ class_name Constants extends Reference
 
 const TITLE_SCENE_PATH = "res://Scenes/Title.tscn"
 const GAME_SCENE_PATH = "res://Scenes/Game.tscn"
-
+const QUIT_SCENE_PATH = "res://Scenes/Quit.tscn"
 
 const MOON_RADIUS :float = 50.0
