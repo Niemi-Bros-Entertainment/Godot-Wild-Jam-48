8675ff9431b8e04e97e3efba7fd0f3a3c1d0bc24
Jonathan Niemi <jontniemi@gmail.com>
2022-08-16 12:42:25 -0400

    Increased jetpack strength

--- Godot-Wild-Jam-48-Project/Scripts/Player/Player.gd
+++ Godot-Wild-Jam-48-Project/Scripts/Player/Player.gd
@@ -13,7 +13,7 @@ const MOON_RADIUS :float = Constants.MOON_RADIUS
 const TRANSFORM_INTERPOLATE :float = 0.2
 const LOOK_PITCH_LIMIT :float = deg2rad(89.0)
 const GRAVITY_STRENGTH :float = 1.0
-const JETPACK_STRENGTH :float = 0.75
+const JETPACK_STRENGTH :float = 0.9
 
 export(float) var speed :float = 7.0
 export(float) var acceleration :float = 5.0
