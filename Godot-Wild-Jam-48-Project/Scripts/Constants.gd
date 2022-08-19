34443f3494a517793ffab29a33ed49e1bfb4b19c
Jonathan Niemi <jontniemi@gmail.com>
2022-08-19 14:52:36 -0400

    Increased cheese point values
    -x10 for all current values

--- Godot-Wild-Jam-48-Project/Scripts/Constants.gd
+++ Godot-Wild-Jam-48-Project/Scripts/Constants.gd
@@ -16,10 +16,10 @@ const SUCCESS_BONUS_POINTS :int = 1000
 const PICKUP_PARTICLE_PREFAB = preload("res://Scenes/Prefabs/VFX/CheesePickupParticle.tscn")
 
 const CHEESE_POINT_LOOKUP :Dictionary = {
-	CheeseType.Swiss: 5,
-	CheeseType.Cheddar: 10,
-	CheeseType.Provolone: 25,
-	CheeseType.Brie: 50,
-	CheeseType.Gouda: 100,
-	CheeseType.Gorgonzola: 500,
+	CheeseType.Swiss: 50,
+	CheeseType.Cheddar: 100,
+	CheeseType.Provolone: 250,
+	CheeseType.Brie: 500,
+	CheeseType.Gouda: 1000,
+	CheeseType.Gorgonzola: 5000,
 }
