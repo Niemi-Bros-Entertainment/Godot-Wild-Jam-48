97e898caf19ff7fb3b40dd5140bc8c3e353a3c09
Jonathan Niemi <jontniemi@gmail.com>
2022-08-20 11:56:50 -0400

    Swapped out dump icon, added sprite3d on control panel
    -when cheese goal is reached, another 3d sprite appears over the control panel
    -using a copy of icon.png for the cheese dump/control panel 3d sprite icons

--- Godot-Wild-Jam-48-Project/Scripts/Entity/ControlPanel.gd
+++ Godot-Wild-Jam-48-Project/Scripts/Entity/ControlPanel.gd
@@ -3,9 +3,11 @@ extends Node
 
 onready var meshInstance :MeshInstance = $ControlPanel
 onready var overlayMat :Material = meshInstance.material_overlay
+onready var sprite3d :Sprite3D = $Sprite3D
 
 
 func _ready():
+	sprite3d.hide()
 	meshInstance.material_overlay = null
 	# warning-ignore:return_value_discarded
 	GameManager.connect("cheese_goal_reached", self, "_on_cheese_goal")
@@ -20,5 +22,6 @@ func operate():
 
 
 func _on_cheese_goal():
+	sprite3d.show()
 	SfxManager.enqueue2d(Enums.SoundType.CheeseGoal)
 	meshInstance.material_overlay = overlayMat
